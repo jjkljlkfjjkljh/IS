65eddd752fb5739bfe8a19ea4677dac679e3369f
Michael Riggs <michaelriggs1@gmail.com>
2018-05-20 02:42:33 -0400

    Initial Project Setup

---
+++ IS/Source/IS/ISGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ISGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IS_API AISGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
